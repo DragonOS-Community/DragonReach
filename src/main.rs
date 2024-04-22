486392644e2685695121bc8062ee32d127489b99
BrahmaMantra <140599389+BrahmaMantra@users.noreply.github.com>
2024-04-22 19:49:51 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,8 @@ use systemctl::listener::Systemctl;
 
 pub struct FileDescriptor(usize);
 
-// const DRAGON_REACH_UNIT_DIR: &'static str = "/etc/reach/system/";
-const DRAGON_REACH_UNIT_DIR: &'static str = "/home/fz/testSystemd/";
+const DRAGON_REACH_UNIT_DIR: &'static str = "/etc/reach/system/";
+//const DRAGON_REACH_UNIT_DIR: &'static str = "/home/fz/testSystemd/";
 
 fn main() {
     // 初始化
