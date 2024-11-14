2585cf5ff1d079731db77ba7b676aa4da6838dcd
val213 <1042594899@qq.com>
2024-11-14 14:44:31 +0800

    fmt

--- src/manager/mod.rs
+++ src/manager/mod.rs
@@ -16,12 +16,10 @@ pub struct Manager;
 impl Manager {
     /// ## 检查当前 DragonReach 运行的项目状态，并对其分发处理
     pub fn check_running_status() {
-        if SIGCHILD_SIGNAL_RECEIVED.compare_exchange(
-            true,
-            false,
-            Ordering::SeqCst,
-            Ordering::SeqCst,
-        ) {
+        if SIGCHILD_SIGNAL_RECEIVED
+            .compare_exchange(true, false, Ordering::SeqCst, Ordering::SeqCst)
+            .is_ok()
+        {
             let mut exited_unit: Vec<(usize, ExitStatus)> = Vec::new();
             let mut running_manager = RUNNING_TABLE.write().unwrap();
             // 检查所有运行中的 Unit
@@ -82,12 +80,10 @@ impl Manager {
 
     /// ## 检查当前所有cmd进程的运行状态
     pub fn check_cmd_proc() {
-        if SIGCHILD_SIGNAL_RECEIVED.compare_exchange(
-            true,
-            false,
-            Ordering::SeqCst,
-            Ordering::SeqCst,
-        ) {
+        if SIGCHILD_SIGNAL_RECEIVED
+            .compare_exchange(true, false, Ordering::SeqCst, Ordering::SeqCst)
+            .is_ok()
+        {
             let mut exited = Vec::new();
             let mut table = CMD_PROCESS_TABLE.write().unwrap();
 
